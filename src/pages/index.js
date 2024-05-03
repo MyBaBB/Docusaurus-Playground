847ad94223e462978a905fd654bae42953346ec9
Brett Baker <GitBaBB@MyBaBB.com>
2024-05-03 05:50:13 -0700

    Prettier and tailwind are formatiing!
    test for a while, Prettier and tailwind format even the md files, on command npm run format,   cool.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,37 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Junk from './markdown-page.md';
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-import Test from './TestjsxFolder/Test.jsx';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Junk from "./markdown-page.md";
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
+import Test from "./TestjsxFolder/Test.jsx";
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttonTrial}>
-          <Link
-             
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          <Link to="/docs/intro">Docusaurus Tutorial - 5min ⏱️</Link>
         </div>
-        <div className="z-50  ">
-          
-      </div>
+        <div className="z-50  "></div>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <Junk />
