f8bca964581dd2c79b3907dd1c26bd28cd7962ac
Brett Baker <GitBaBB@MyBaBB.com>
2024-05-04 07:40:57 -0700

    letter color not specified and was black, fixed and added text shadow

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,14 +12,14 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
+      <div className="container containerTop ">
         <h1 className="homeTitle p-4 font-Wallpoet text-sm xxs:text-[19px] xs:text-[24px] sm:text-[35px] font-extrabold text-lime-200 whitespace-nowrap ">
           Component Playground
             
         </h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div>< Gorilla /></div>
-        <div className='buttonTrial p-4   text-[12px] xs:text-[20px] whitespace-nowrap'>
+        <div className='buttonTrial p-4 text-white  text-[12px] xs:text-[20px] whitespace-nowrap'>
           <Link to="/docs/intro">Docusaurus Tutorial - 5min ⏱️</Link>
         </div>
         
